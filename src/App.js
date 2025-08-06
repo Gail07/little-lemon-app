b493890d2f6b7cfd3c0a769ad9f37a6fbbc7bf0d
Kacou Abigail <kacouabigail0@gmail.com>
2025-08-06 16:17:27 +0000

    Added CSS styles to the navbar, modified navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Home from "./Components/pages/Home";
-import Main from "./Components/Main";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Main />
-      <Home />
-      <Footer />
     </>
   );
   }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,25 @@
+import logo from "../images/logo-little-lemon.png";
+import menuIcon from "../images/menu-icon.svg";
+import React, { useState } from 'react';
+
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+        const toggleMenu = () => {
+            setMenuOpen(!menuOpen);
+        }
+
+
     return (
-        <nav className="navbar">
-            <img src="/src/images/logo little lemon.png" alt="Little Lemon Logo" className="logo" width="100" height="100" />
-            <ul>
+        <nav className= {`navbar ${menuOpen ? 'open' : ''}`}>
+            <div className="logo">
+                <img src={logo} alt="little lemon logo" />
+            </div>
+
+            <div className="menu-icon" onClick={toggleMenu}>
+                <img src={menuIcon} alt="menu-icon" />
+            </div>
+
+            <ul className={`nav-links ${menuOpen ? 'visible' : ''}`}>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#reservation">Reservation</a></li>
